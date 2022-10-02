445527fabef2ca29c4fd8adb36f7d961f1cca350
AlonsoAlfa <afaustoalonso@gmail.com>
2022-10-02 16:56:13 -0500

    This is the preview of my verbs project

---
+++ verbs.js
@@ -0,0 +1,31 @@
+
+
+const verbs = [
+'Ayudar',	
+'Estimar',
+'Querer',
+'Regalar',
+'Amar',	
+'Apreciar',
+'Abrazar',	
+'Comprender',
+'Sonreír',	
+'Proteger'
+
+];
+
+
+
+
+const verbos = [
+'Help',
+'Estimate',
+'Want',
+'Give away',
+'Love',
+'Cherish',
+'Embrace',
+'Understand',
+'Smile',
+'To protect'
+];
